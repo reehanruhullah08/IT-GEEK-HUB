160c62fdd7bc1f471b1175dbf51669643958790a
reehanruhullah08 <reehanruhullah8@gmail.com>
2022-05-22 20:34:28 +0530

    all files

---
+++ project/possible_question/poss_ques.js
@@ -0,0 +1,24 @@
+const body=document.querySelector("body"),
+     nav=document.querySelector("nav"),
+     modetoggle= document.querySelector(".dark-light"),
+     sidebaropen=document.querySelector(".sidebaropen"),
+     sidebarclose=document.querySelector(".sidebarclose");
+     C2=document.querySelector(".C2");
+
+    
+    
+     modetoggle.addEventListener("click" , () =>{
+    modetoggle.classList.toggle("active");
+    body.classList.toggle("dark");
+     } ) 
+
+     //to toggle sidebar
+     sidebaropen.addEventListener("click" , () =>{
+        nav.classList.add("active");
+    })
+    body.addEventListener("click" , e =>{
+        let clickedelm=e.target;
+        if(!clickedelm.classList.contains("sidebaropen")&& !clickedelm.classList.contains("menu")){
+            nav.classList.remove("active");
+        }
+    })
